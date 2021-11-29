af3e3d3758cb0cc4c9105e92a5497cccda8068a1
Sarah Marshall <sarah.marshall@microsoft.com>
2021-11-29 12:40:08 -0800

    Find function with EntryPoint attribute

--- pyqir-jit/src/jit.rs
+++ pyqir-jit/src/jit.rs
@@ -1,60 +1,87 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-use qirlib::context::{Context, ContextType};
-use crate::runtime::Simulator;
-use crate::interop::SemanticModel;
-use inkwell::targets::TargetMachine;
+use crate::{interop::SemanticModel, runtime::Simulator};
 use inkwell::{
-    passes::PassManagerBuilder,
-    targets::{InitializationConfig, Target},
-    OptimizationLevel,
+    attributes::AttributeLoc,
+    module::Module,
+    targets::{InitializationConfig, Target, TargetMachine},
+    values::FunctionValue,
 };
 use microsoft_quantum_qir_runtime_sys::runtime::BasicRuntimeDriver;
-use qirlib::passes::run_basic_passes_on;
+use qirlib::{
+    context::{Context, ContextType},
+    passes::run_basic_passes_on,
+};
 
 pub fn run_module(module: String) -> Result<SemanticModel, String> {
     let ctx = inkwell::context::Context::create();
     let context_type = ContextType::File(&module);
     let context = Context::new(&ctx, context_type)?;
-    let model = run_ctx(context)?;
-    Ok(model)
+    run_ctx(context)
 }
 
 pub fn run_ctx<'ctx>(context: Context<'ctx>) -> Result<SemanticModel, String> {
     Target::initialize_native(&InitializationConfig::default()).unwrap();
-
     let default_triple = TargetMachine::get_default_triple();
 
     let target = Target::from_triple(&default_triple).expect("Unable to create target machine");
-
     assert!(target.has_asm_backend());
     assert!(target.has_target_machine());
 
     run_basic_passes_on(&context);
 
+    let entry_point = module_functions(&context.module)
+        .filter(is_entry_point)
+        .next()
+        .ok_or("Module contains no matching entry point.")?;
+
+    let entry_point_name = entry_point.get_name().to_str().unwrap();
+
     unsafe {
         BasicRuntimeDriver::initialize_qir_context(true);
         let _ = microsoft_quantum_qir_runtime_sys::foundation::QSharpFoundation::new();
-
         let _ = inkwell::support::load_library_permanently("");
         let simulator = Simulator::new(&context, &context.execution_engine);
+
         let main = context
             .execution_engine
-            .get_function::<unsafe extern "C" fn() -> ()>("QuantumApplication__Run")
+            .get_function::<unsafe extern "C" fn() -> ()>(entry_point_name)
             .unwrap();
+
         main.call();
         Ok(simulator.get_model())
     }
 }
 
+fn is_entry_point(function: &FunctionValue) -> bool {
+    function
+        .get_string_attribute(AttributeLoc::Function, "EntryPoint")
+        .is_some()
+}
+
+fn module_functions<'ctx>(module: &Module<'ctx>) -> impl Iterator<Item = FunctionValue<'ctx>> {
+    struct FunctionValueIter<'ctx>(Option<FunctionValue<'ctx>>);
+
+    impl<'ctx> Iterator for FunctionValueIter<'ctx> {
+        type Item = FunctionValue<'ctx>;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let function = self.0;
+            self.0 = function.and_then(|f| f.get_next_function());
+            function
+        }
+    }
+
+    FunctionValueIter(module.get_first_function())
+}
+
 #[cfg(test)]
 mod tests {
-
+    use super::run_module;
     use crate::interop::{ClassicalRegister, Measured, QuantumRegister, SemanticModel};
     use crate::interop::{Controlled, Instruction, Single};
     use tempfile::tempdir;
-    use super::run_ctx;
 
     #[ignore = "CI Requires runtime recompilation"]
     #[test]
@@ -94,5 +121,11 @@ mod tests {
         //let context = pyqir_generator::populate_context(&ctx, &model).unwrap();
         //let model = run_ctx(context)?;
         Ok(model.clone())
+    }
+
+    #[test]
+    fn eval_my_qir_file() {
+        let path = "C:\\Users\\samarsha\\Code\\samarsha\\qsharp-sandbox\\App\\qir\\App.ll";
+        run_module(path.to_string()).unwrap();
     }
 }
