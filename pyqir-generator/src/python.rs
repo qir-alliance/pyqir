ca0c441f24a3362e5861fcbf32b50e152c2143a9
Sarah Marshall <sarah.marshall@microsoft.com>
2022-01-31 17:26:08 -0800

    Add skeleton Rust pyclasses for new API

--- pyqir-generator/src/python.rs
+++ pyqir-generator/src/python.rs
@@ -198,3 +198,170 @@ impl Module {
 fn enable_logging() -> PyResult<()> {
     env_logger::try_init().map_err(|e| PyOSError::new_err(e.to_string()))
 }
+
+const RESULT_NAME: &str = "result";
+const QUBIT_NAME: &str = "qubit";
+
+#[pyclass]
+struct SimpleModule {
+    builder: Py<Builder>,
+}
+
+#[pymethods]
+impl SimpleModule {
+    #[new]
+    fn new(name: String, num_qubits: u64, num_results: u64) -> PyResult<SimpleModule> {
+        let registers = vec![ClassicalRegister::new(RESULT_NAME.to_string(), num_results)];
+
+        let qubits = (0..num_qubits)
+            .map(|i| QuantumRegister::new(QUBIT_NAME.to_string(), i))
+            .collect();
+
+        let model = SemanticModel {
+            name,
+            registers,
+            qubits,
+            instructions: Vec::new(),
+        };
+
+        Python::with_gil(|py| {
+            let builder = Py::new(py, Builder { model })?;
+            Ok(SimpleModule { builder })
+        })
+    }
+
+    #[getter]
+    fn qubits(&self) -> PyResult<Vec<Qubit>> {
+        Python::with_gil(|py| {
+            let builder = self.builder.as_ref(py).try_borrow()?;
+            Ok(builder
+                .model
+                .qubits
+                .iter()
+                .map(|q| Qubit { index: q.index })
+                .collect())
+        })
+    }
+
+    #[getter]
+    fn results(&self) -> PyResult<Vec<Ref>> {
+        Python::with_gil(|py| {
+            let builder = self.builder.as_ref(py).try_borrow()?;
+            let size = builder.model.registers.first().unwrap().size;
+
+            Ok((0..size)
+                .map(|index| Ref(RefKind::Result { index }))
+                .collect())
+        })
+    }
+
+    #[getter]
+    fn builder(&self) -> Py<Builder> {
+        self.builder.clone()
+    }
+
+    fn ir(&self) -> PyResult<String> {
+        Python::with_gil(|py| {
+            let builder = self.builder.as_ref(py).try_borrow()?;
+            get_ir_string(&builder.model).map_err(PyOSError::new_err)
+        })
+    }
+
+    fn bitcode(&self) -> &[u8] {
+        todo!()
+    }
+}
+
+#[pyclass]
+struct Qubit {
+    index: u64,
+}
+
+#[pyclass]
+struct Ref(RefKind);
+
+enum RefKind {
+    Result { index: u64 },
+}
+
+#[pyclass]
+struct Builder {
+    model: SemanticModel,
+}
+
+#[pyclass]
+struct BasicQisBuilder {
+    builder: Py<Builder>,
+}
+
+#[pymethods]
+impl BasicQisBuilder {
+    #[new]
+    fn new(builder: Py<Builder>) -> Self {
+        BasicQisBuilder { builder }
+    }
+
+    fn cx(&self, control: &Qubit, target: &Qubit) {
+        todo!()
+    }
+
+    fn cz(&self, control: &Qubit, target: &Qubit) {
+        todo!()
+    }
+
+    fn h(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn m(&self, qubit: &Qubit, result: &Ref) {
+        todo!()
+    }
+
+    fn reset(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn rx(&self, theta: f64, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn ry(&self, theta: f64, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn rz(&self, theta: f64, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn s(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn s_adj(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn t(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn t_adj(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn x(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn y(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn z(&self, qubit: &Qubit) {
+        todo!()
+    }
+
+    fn if_result(&self, result: &Ref, one: &PyAny, zero: &PyAny) {
+        todo!()
+    }
+}
