3b4c8a6323deb9e8470880510d2a28c722321995
Sarah Marshall <sarah.marshall@microsoft.com>
2022-02-02 15:31:36 -0800

    Update examples

--- examples/generator/bell_pair.py
+++ examples/generator/bell_pair.py
@@ -1,27 +1,20 @@
 #!/usr/bin/env python3
 
-# Copyright(c) Microsoft Corporation.
+# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from pyqir_generator.instruction import *
-from pyqir_generator.module import Module, Register
+from pyqir_generator.module import SimpleModule
+from pyqir_generator.qis import BasicQisBuilder
 
-# This operation creates a Bell pair and returns the result of measuring each
+# This module creates a Bell pair and returns the result of measuring each
 # qubit.
-instructions = [
-    H("qubit0"),
-    Cx("qubit0", "qubit1"),
+bell = SimpleModule("bell", num_qubits=2, num_results=2)
+qis = BasicQisBuilder(bell.builder)
 
-    M("qubit0", "output0"),
-    M("qubit1", "output1"),
-]
-
-bell_pair = Module(
-    name="Bell",
-    bits=[Register("output", 2)],
-    qubits=[Register("qubit", 2)],
-    instructions=instructions
-)
+qis.h(bell.qubits[0])
+qis.cx(bell.qubits[0], bell.qubits[1])
+qis.m(bell.qubits[0], bell.results[0])
+qis.m(bell.qubits[1], bell.results[1])
 
 if __name__ == "__main__":
-    print(bell_pair.ir())
+    print(bell.ir())
