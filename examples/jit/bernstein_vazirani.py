75a85867d9271352339d1a4788fb687580786524
Ian Davis <ian.f.davis@microsoft.com>
2022-02-04 17:02:26 -0800

    Updating JIT to use new reduced profile set

--- examples/jit/bernstein_vazirani.py
+++ examples/jit/bernstein_vazirani.py
@@ -14,8 +14,6 @@ file = os.path.join(path, "bernstein_vazirani.bc")
 jit = NonadaptiveJit()
 logger = GateLogger()
 
-print("# NonadaptiveJit output returning the uninitialized output", flush=True)
-
 jit.eval(file, logger)
 
 print("# output from GateLogger", flush=True)
--- pyqir-jit/src/runtime.rs
+++ pyqir-jit/src/runtime.rs
@@ -35,181 +35,117 @@ impl<'ctx> Simulator {
 
     fn bind(module: &Module<'ctx>, ee: &ExecutionEngine<'ctx>) {
         let intrinsics = Intrinsics::new(module);
-
-        if let Some(ins) = intrinsics.h_ins {
+        if let Some(ins) = intrinsics.cnot {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__cnot__body as usize);
+        }
+        if let Some(ins) = intrinsics.cz {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__cz__body as usize);
+        }
+        if let Some(ins) = intrinsics.h {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__h__body as usize);
         }
-
-        if let Some(ins) = intrinsics.h_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__h__ctl as usize);
-        }
-        if let Some(ins) = intrinsics.m_ins {
-            ee.add_global_mapping(
-                &ins,
-                super::intrinsics::__quantum__qis__measure__body as usize,
-            );
-        }
-        if let Some(ins) = intrinsics.r_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__r__body as usize);
-        }
-        if let Some(ins) = intrinsics.r_adj_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__r__adj as usize);
-        }
-        if let Some(ins) = intrinsics.r_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__r__ctl as usize);
-        }
-        if let Some(ins) = intrinsics.r_ctl_adj_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__r__ctladj as usize);
-        }
-
-        if let Some(ins) = intrinsics.s_ins {
+        if let Some(ins) = intrinsics.s {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__s__body as usize);
         }
-        if let Some(ins) = intrinsics.s_adj_ins {
+        if let Some(ins) = intrinsics.s_adj {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__s__adj as usize);
         }
-        if let Some(ins) = intrinsics.s_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__s__ctl as usize);
-        }
-        if let Some(ins) = intrinsics.s_ctl_adj_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__s__ctladj as usize);
-        }
-
-        if let Some(ins) = intrinsics.t_ins {
+        if let Some(ins) = intrinsics.t {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__t__body as usize);
         }
-        if let Some(ins) = intrinsics.t_adj_ins {
+        if let Some(ins) = intrinsics.t_adj {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__t__adj as usize);
         }
-        if let Some(ins) = intrinsics.t_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__t__ctl as usize);
-        }
-        if let Some(ins) = intrinsics.t_ctl_adj_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__t__ctladj as usize);
-        }
-
-        if let Some(ins) = intrinsics.x_ins {
+        if let Some(ins) = intrinsics.x {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__x__body as usize);
         }
-        if let Some(ins) = intrinsics.x_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__x__ctl as usize);
-        }
-
-        if let Some(ins) = intrinsics.y_ins {
+        if let Some(ins) = intrinsics.y {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__y__body as usize);
         }
-        if let Some(ins) = intrinsics.y_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__y__ctl as usize);
-        }
-
-        if let Some(ins) = intrinsics.z_ins {
+        if let Some(ins) = intrinsics.z {
             ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__z__body as usize);
         }
-        if let Some(ins) = intrinsics.z_ctl_ins {
-            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__z__ctl as usize);
+        if let Some(ins) = intrinsics.r_x {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__rx__body as usize);
+        }
+        if let Some(ins) = intrinsics.r_y {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__ry__body as usize);
+        }
+        if let Some(ins) = intrinsics.r_z {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__qis__rz__body as usize);
+        }
+        if let Some(ins) = intrinsics.reset {
+            ee.add_global_mapping(
+                &ins,
+                super::intrinsics::__quantum__qis__reset__body as usize,
+            );
+        }
+
+        if let Some(ins) = intrinsics.m {
+            ee.add_global_mapping(
+                &ins,
+                super::intrinsics::__quantum__qis__m__body as usize,
+            );
+        }
+
+        let runtime = Runtime::new(module);
+        if let Some(ins) = runtime.result_get_one {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__rt__result_get_one as usize);
+        }
+        if let Some(ins) = runtime.result_get_zero {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__rt__result_get_zero as usize);
+        }
+        if let Some(ins) = runtime.result_equal {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__rt__result_equal as usize);
+        }
+        if let Some(ins) = runtime.qubit_allocate {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__rt__qubit_allocate as usize);
+        }
+        if let Some(ins) = runtime.qubit_release {
+            ee.add_global_mapping(&ins, super::intrinsics::__quantum__rt__qubit_release as usize);
         }
     }
 }
 
 pub struct Intrinsics<'ctx> {
+    pub cnot: Option<FunctionValue<'ctx>>,
+    pub cz: Option<FunctionValue<'ctx>>,
     pub m: Option<FunctionValue<'ctx>>,
-    pub m_ins: Option<FunctionValue<'ctx>>,
     pub r_x: Option<FunctionValue<'ctx>>,
     pub r_y: Option<FunctionValue<'ctx>>,
     pub r_z: Option<FunctionValue<'ctx>>,
-    pub r_ins: Option<FunctionValue<'ctx>>,
-    pub r_adj_ins: Option<FunctionValue<'ctx>>,
-    pub r_ctl_ins: Option<FunctionValue<'ctx>>,
-    pub r_ctl_adj_ins: Option<FunctionValue<'ctx>>,
     pub reset: Option<FunctionValue<'ctx>>,
     pub h: Option<FunctionValue<'ctx>>,
-    pub h_ins: Option<FunctionValue<'ctx>>,
-    pub h_ctl_ins: Option<FunctionValue<'ctx>>,
     pub x: Option<FunctionValue<'ctx>>,
-    pub x_ins: Option<FunctionValue<'ctx>>,
-    pub x_ctl: Option<FunctionValue<'ctx>>,
-    pub x_ctl_ins: Option<FunctionValue<'ctx>>,
     pub y: Option<FunctionValue<'ctx>>,
-    pub y_ins: Option<FunctionValue<'ctx>>,
-    pub y_ctl: Option<FunctionValue<'ctx>>,
-    pub y_ctl_ins: Option<FunctionValue<'ctx>>,
     pub z: Option<FunctionValue<'ctx>>,
-    pub z_ins: Option<FunctionValue<'ctx>>,
-    pub z_ctl: Option<FunctionValue<'ctx>>,
-    pub z_ctl_ins: Option<FunctionValue<'ctx>>,
     pub s: Option<FunctionValue<'ctx>>,
-    pub s_ins: Option<FunctionValue<'ctx>>,
     pub s_adj: Option<FunctionValue<'ctx>>,
-    pub s_adj_ins: Option<FunctionValue<'ctx>>,
-    pub s_ctl_ins: Option<FunctionValue<'ctx>>,
-    pub s_ctl_adj_ins: Option<FunctionValue<'ctx>>,
     pub t: Option<FunctionValue<'ctx>>,
-    pub t_ins: Option<FunctionValue<'ctx>>,
     pub t_adj: Option<FunctionValue<'ctx>>,
-    pub t_adj_ins: Option<FunctionValue<'ctx>>,
-    pub t_ctl_ins: Option<FunctionValue<'ctx>>,
-    pub t_ctl_adj_ins: Option<FunctionValue<'ctx>>,
 }
 
 impl<'ctx> Intrinsics<'ctx> {
     pub fn new(module: &Module<'ctx>) -> Self {
         let intrinsics = Intrinsics {
-            m: Intrinsics::get_mqi_body(module, "M"),
-            m_ins: Intrinsics::get_qis_intrinsic_function_body(module, "measure"),
-            r_x: Intrinsics::get_mqi_body(module, "Rx"),
-            r_y: Intrinsics::get_mqi_body(module, "Ry"),
-            r_z: Intrinsics::get_mqi_body(module, "Rz"),
-            r_ins: Intrinsics::get_qis_intrinsic_function_body(module, "r"),
-            r_adj_ins: Intrinsics::get_qis_intrinsic_function_adj(module, "r"),
-            r_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "r"),
-            r_ctl_adj_ins: Intrinsics::get_qis_intrinsic_function_ctladj(module, "r"),
-            reset: Intrinsics::get_mqi_body(module, "Reset"),
-            h: Intrinsics::get_mqi_body(module, "H"),
-            h_ins: Intrinsics::get_qis_intrinsic_function_body(module, "H"),
-            h_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "H"),
-            x: Intrinsics::get_mqi_body(module, "X"),
-            x_ins: Intrinsics::get_qis_intrinsic_function_body(module, "X"),
-            x_ctl: Intrinsics::get_mqi_ctl(module, "X"),
-            x_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "X"),
-            y: Intrinsics::get_mqi_body(module, "Y"),
-            y_ins: Intrinsics::get_qis_intrinsic_function_body(module, "Y"),
-            y_ctl: Intrinsics::get_mqi_ctl(module, "Y"),
-            y_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "Y"),
-            z: Intrinsics::get_mqi_body(module, "Z"),
-            z_ins: Intrinsics::get_qis_intrinsic_function_body(module, "Z"),
-            z_ctl: Intrinsics::get_mqi_ctl(module, "Z"),
-            z_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "Z"),
-            s: Intrinsics::get_mqi_body(module, "S"),
-            s_ins: Intrinsics::get_qis_intrinsic_function_body(module, "S"),
-            s_adj: Intrinsics::get_mqi_adj(module, "S"),
-            s_adj_ins: Intrinsics::get_qis_intrinsic_function_adj(module, "S"),
-            s_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "S"),
-            s_ctl_adj_ins: Intrinsics::get_qis_intrinsic_function_ctladj(module, "S"),
-            t: Intrinsics::get_mqi_body(module, "T"),
-            t_ins: Intrinsics::get_qis_intrinsic_function_body(module, "T"),
-            t_adj: Intrinsics::get_mqi_adj(module, "T"),
-            t_adj_ins: Intrinsics::get_qis_intrinsic_function_adj(module, "T"),
-            t_ctl_ins: Intrinsics::get_qis_intrinsic_function_ctl(module, "T"),
-            t_ctl_adj_ins: Intrinsics::get_qis_intrinsic_function_ctladj(module, "T"),
+            cnot: Intrinsics::get_qis_intrinsic_function_body(module, "cnot"),
+            cz: Intrinsics::get_qis_intrinsic_function_body(module, "Cz"),
+            m: Intrinsics::get_qis_intrinsic_function_body(module, "M"),
+            r_x: Intrinsics::get_qis_intrinsic_function_body(module, "Rx"),
+            r_y: Intrinsics::get_qis_intrinsic_function_body(module, "Ry"),
+            r_z: Intrinsics::get_qis_intrinsic_function_body(module, "Rz"),
+            reset: Intrinsics::get_qis_intrinsic_function_body(module, "Reset"),
+            h: Intrinsics::get_qis_intrinsic_function_body(module, "H"),
+            x: Intrinsics::get_qis_intrinsic_function_body(module, "X"),
+            y: Intrinsics::get_qis_intrinsic_function_body(module, "Y"),
+            z: Intrinsics::get_qis_intrinsic_function_body(module, "Z"),
+            s: Intrinsics::get_qis_intrinsic_function_body(module, "S"),
+            s_adj: Intrinsics::get_qis_intrinsic_function_adj(module, "S"),
+            t: Intrinsics::get_qis_intrinsic_function_body(module, "T"),
+            t_adj: Intrinsics::get_qis_intrinsic_function_adj(module, "T"),
         };
 
         intrinsics
-    }
-
-    fn get_qis_intrinsic_function_ctl(
-        module: &Module<'ctx>,
-        name: &str,
-    ) -> Option<FunctionValue<'ctx>> {
-        let function_name = format!("__quantum__qis__{}__ctl", name.to_lowercase());
-        Intrinsics::get_function(module, function_name.as_str())
-    }
-
-    fn get_qis_intrinsic_function_ctladj(
-        module: &Module<'ctx>,
-        name: &str,
-    ) -> Option<FunctionValue<'ctx>> {
-        let function_name = format!("__quantum__qis__{}__ctladj", name.to_lowercase());
-        Intrinsics::get_function(module, function_name.as_str())
     }
 
     fn get_qis_intrinsic_function_body(
@@ -217,7 +153,7 @@ impl<'ctx> Intrinsics<'ctx> {
         name: &str,
     ) -> Option<FunctionValue<'ctx>> {
         let function_name = format!("__quantum__qis__{}__body", name.to_lowercase());
-        Intrinsics::get_function(module, function_name.as_str())
+        get_function(module, function_name.as_str())
     }
 
     fn get_qis_intrinsic_function_adj(
@@ -225,35 +161,50 @@ impl<'ctx> Intrinsics<'ctx> {
         name: &str,
     ) -> Option<FunctionValue<'ctx>> {
         let function_name = format!("__quantum__qis__{}__adj", name.to_lowercase());
-        Intrinsics::get_function(module, function_name.as_str())
+        get_function(module, function_name.as_str())
+    }
+}
+
+pub struct Runtime<'ctx> {
+    pub result_get_one: Option<FunctionValue<'ctx>>,
+    pub result_get_zero: Option<FunctionValue<'ctx>>,
+    pub result_equal: Option<FunctionValue<'ctx>>,
+    pub qubit_allocate: Option<FunctionValue<'ctx>>,
+    pub qubit_release: Option<FunctionValue<'ctx>>,
+}
+
+impl<'ctx> Runtime<'ctx> {
+    pub fn new(module: &Module<'ctx>) -> Self {
+        let intrinsics = Runtime {
+            result_get_one: Runtime::get_rt_intrinsic_function_body(module, "result_get_one"),
+            result_get_zero: Runtime::get_rt_intrinsic_function_body(module, "result_get_zero"),
+            result_equal: Runtime::get_rt_intrinsic_function_body(module, "result_equal"),
+            qubit_allocate: Runtime::get_rt_intrinsic_function_body(module, "qubit_allocate"),
+            qubit_release: Runtime::get_rt_intrinsic_function_body(module, "qubit_release"),
+        };
+
+        intrinsics
     }
 
-    fn get_mqi_body(module: &Module<'ctx>, name: &str) -> Option<FunctionValue<'ctx>> {
-        let function_name = format!("Microsoft__Quantum__Intrinsic__{}__body", name);
-        Intrinsics::get_function(module, function_name.as_str())
+    fn get_rt_intrinsic_function_body(
+        module: &Module<'ctx>,
+        name: &str,
+    ) -> Option<FunctionValue<'ctx>> {
+        let function_name = format!("__quantum__rt__{}", name.to_lowercase());
+        get_function(module, function_name.as_str())
     }
+}
 
-    fn get_mqi_ctl(module: &Module<'ctx>, name: &str) -> Option<FunctionValue<'ctx>> {
-        let function_name = format!("Microsoft__Quantum__Intrinsic__{}__ctl", name);
-        Intrinsics::get_function(module, function_name.as_str())
-    }
-
-    fn get_mqi_adj(module: &Module<'ctx>, name: &str) -> Option<FunctionValue<'ctx>> {
-        let function_name = format!("Microsoft__Quantum__Intrinsic__{}__adj", name);
-        Intrinsics::get_function(module, function_name.as_str())
-    }
-
-    fn get_function(module: &Module<'ctx>, function_name: &str) -> Option<FunctionValue<'ctx>> {
-        let defined_function = module.get_function(function_name);
-        match defined_function {
-            None => {
-                log::debug!(
-                    "{} global function was not defined in the module",
-                    function_name
-                );
-                None
-            }
-            Some(value) => Some(value),
+fn get_function<'ctx>(module: &Module<'ctx>, function_name: &str) -> Option<FunctionValue<'ctx>> {
+    let defined_function = module.get_function(function_name);
+    match defined_function {
+        None => {
+            log::debug!(
+                "{} global function was not defined in the module",
+                function_name
+            );
+            None
         }
+        Some(value) => Some(value),
     }
 }
