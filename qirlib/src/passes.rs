0eb327bc4401060b9ba23cdb64d096ce10f6cf2d
Sarah Marshall <sarah.marshall@microsoft.com>
2022-02-10 12:52:37 -0800

    Revert removing add_global_dce_pass

--- qirlib/src/passes.rs
+++ qirlib/src/passes.rs
@@ -10,8 +10,7 @@ pub fn run_basic_passes_on(module: &Module) -> bool {
     let pass_manager_builder = PassManagerBuilder::create();
     pass_manager_builder.set_optimization_level(OptimizationLevel::None);
     let fpm = PassManager::create(());
-    // TODO: This breaks PyQIR JIT.
-    // fpm.add_global_dce_pass();
+    fpm.add_global_dce_pass();
     fpm.add_strip_dead_prototypes_pass();
     pass_manager_builder.populate_module_pass_manager(&fpm);
     fpm.run_on(module)
