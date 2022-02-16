7cbb1cc2aec99343f36135cf8d5d03c7688a38a5
Sarah Marshall <33814365+samarsha@users.noreply.github.com>
2022-02-16 10:58:56 -0800

    Update examples/jit/bernstein_vazirani.py
    Co-authored-by: Cassandra Granade <chgranad@microsoft.com>

--- examples/jit/bernstein_vazirani.py
+++ examples/jit/bernstein_vazirani.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright(c) Microsoft Corporation.
+# Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
 from pyqir.jit.nonadaptivejit import NonadaptiveJit
