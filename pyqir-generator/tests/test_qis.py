59405fdc3eddcd10875f5d5e89aec5c61432b286
Sarah Marshall <sarah.marshall@microsoft.com>
2022-03-18 15:00:14 -0700

    Use subtests

--- pyqir-generator/tests/test_qis.py
+++ pyqir-generator/tests/test_qis.py
@@ -6,117 +6,71 @@ import unittest
 
 
 class QisTest(unittest.TestCase):
-    def test_cx(self) -> None:
-        mod = SimpleModule("test", 2, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.cx(mod.qubits[0], mod.qubits[1])
-        self.assertIn(_controlled("cnot"), mod.ir())
+    def test_single(self) -> None:
+        cases = [
+            ("h", lambda qis: qis.h),
+            ("reset", lambda qis: qis.reset),
+            ("s", lambda qis: qis.s),
+            ("t", lambda qis: qis.t),
+            ("x", lambda qis: qis.x),
+            ("y", lambda qis: qis.y),
+            ("z", lambda qis: qis.z),
+        ]
 
-    def test_cz(self) -> None:
-        mod = SimpleModule("test", 2, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.cz(mod.qubits[0], mod.qubits[1])
-        self.assertIn(_controlled("cz"), mod.ir())
+        for name, gate in cases:
+            with self.subTest(name):
+                mod = SimpleModule("test_single", 1, 0)
+                qis = BasicQisBuilder(mod.builder)
+                gate(qis)(mod.qubits[0])
+                call = f"call void @__quantum__qis__{name}__body(%Qubit* null)"
+                self.assertIn(call, mod.ir())
 
-    def test_h(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.h(mod.qubits[0])
-        self.assertIn(_single("h"), mod.ir())
+    def test_controlled(self) -> None:
+        cases = [
+            ("cnot", lambda qis: qis.cx),
+            ("cz", lambda qis: qis.cz),
+        ]
+
+        for name, gate in cases:
+            with self.subTest(name):
+                mod = SimpleModule("test_controlled", 2, 0)
+                qis = BasicQisBuilder(mod.builder)
+                gate(qis)(mod.qubits[0], mod.qubits[1])
+                call = f"call void @__quantum__qis__{name}__body(%Qubit* null, %Qubit* inttoptr (i64 1 to %Qubit*))"
+                self.assertIn(call, mod.ir())
+
+    def test_adjoint(self) -> None:
+        cases = [
+            ("s", lambda qis: qis.s_adj),
+            ("t", lambda qis: qis.t_adj),
+        ]
+
+        for name, gate in cases:
+            with self.subTest(name):
+                mod = SimpleModule("test_adjoint", 1, 0)
+                qis = BasicQisBuilder(mod.builder)
+                gate(qis)(mod.qubits[0])
+                call = f"call void @__quantum__qis__{name}__adj(%Qubit* null)"
+                self.assertIn(call, mod.ir())
+
+    def test_rotated(self) -> None:
+        cases = [
+            ("rx", lambda qis: qis.rx),
+            ("ry", lambda qis: qis.ry),
+            ("rz", lambda qis: qis.rz),
+        ]
+
+        for name, gate in cases:
+            with self.subTest(name):
+                mod = SimpleModule("test_rotated", 1, 0)
+                qis = BasicQisBuilder(mod.builder)
+                gate(qis)(0.0, mod.qubits[0])
+                call = f"call void @__quantum__qis__{name}__body(double 0.000000e+00, %Qubit* null)"
+                self.assertIn(call, mod.ir())
 
     def test_m(self) -> None:
-        mod = SimpleModule("test", 1, 1)
+        mod = SimpleModule("test_m", 1, 1)
         qis = BasicQisBuilder(mod.builder)
         qis.m(mod.qubits[0], mod.results[0])
-        self.assertIn(_measured("m"), mod.ir())
-
-    def test_reset(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.reset(mod.qubits[0])
-        self.assertIn(_single("reset"), mod.ir())
-
-    def test_rx(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.rx(0.0, mod.qubits[0])
-        self.assertIn(_rotated("rx"), mod.ir())
-
-    def test_ry(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.ry(0.0, mod.qubits[0])
-        self.assertIn(_rotated("ry"), mod.ir())
-
-    def test_rz(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.rz(0.0, mod.qubits[0])
-        self.assertIn(_rotated("rz"), mod.ir())
-
-    def test_s(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.s(mod.qubits[0])
-        self.assertIn(_single("s"), mod.ir())
-
-    def test_s_adj(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.s_adj(mod.qubits[0])
-        self.assertIn(_adjoint("s"), mod.ir())
-
-    def test_t(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.t(mod.qubits[0])
-        self.assertIn(_single("t"), mod.ir())
-
-    def test_t_adj(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.t_adj(mod.qubits[0])
-        self.assertIn(_adjoint("t"), mod.ir())
-
-    def test_x(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.x(mod.qubits[0])
-        self.assertIn(_single("x"), mod.ir())
-
-    def test_y(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.y(mod.qubits[0])
-        self.assertIn(_single("y"), mod.ir())
-
-    def test_z(self) -> None:
-        mod = SimpleModule("test", 1, 0)
-        qis = BasicQisBuilder(mod.builder)
-        qis.z(mod.qubits[0])
-        self.assertIn(_single("z"), mod.ir())
-
-
-def _single(gate: str) -> str:
-    call = f"call void @__quantum__qis__{gate}__body"
-    return f"{call}(%Qubit* null)"
-
-
-def _controlled(gate: str) -> str:
-    call = f"call void @__quantum__qis__{gate}__body"
-    return f"{call}(%Qubit* null, %Qubit* inttoptr (i64 1 to %Qubit*))"
-
-
-def _adjoint(gate: str) -> str:
-    call = f"call void @__quantum__qis__{gate}__adj"
-    return f"{call}(%Qubit* null)"
-
-
-def _rotated(gate: str) -> str:
-    call = f"call void @__quantum__qis__{gate}__body"
-    return f"{call}(double 0.000000e+00, %Qubit* null)"
-
-
-def _measured(gate: str) -> str:
-    call = f"call %Result* @__quantum__qis__{gate}__body"
-    return f"{call}(%Qubit* null)"
+        call = f"call %Result* @__quantum__qis__m__body(%Qubit* null)"
+        self.assertIn(call, mod.ir())
