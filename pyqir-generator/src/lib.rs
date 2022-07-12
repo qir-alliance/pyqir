bbb8e003faad997fe5dc3bd0f89692215d838f86
Ian Davis <ian.f.davis@microsoft.com>
2022-07-12 12:15:55 -0700

    Ignoring 1.64 lint until we update to python 3.7+

--- pyqir-generator/src/lib.rs
+++ pyqir-generator/src/lib.rs
@@ -10,5 +10,9 @@
 //     we can directly control in our code.
 #![allow(clippy::needless_option_as_deref)]
 
+// This was introduced in 1.62, but we can't update the dependency to
+// to resolve it until we move to a newer version of python.
+#![allow(clippy::format_push_string)]
+
 #[cfg(feature = "python-bindings")]
 pub mod python;
