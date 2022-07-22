148ad9ef9c611f2ed001e55815a9f3bbd8d599d6
Sarah Marshall <sarah.marshall@microsoft.com>
2022-07-21 17:15:34 -0700

    Add tests for unimplemented behavior

--- pyqir-generator/tests/test_int_intrinsics.py
+++ pyqir-generator/tests/test_int_intrinsics.py
@@ -1,38 +1,39 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-from pyqir.generator import SimpleModule, types
+from typing import Callable, List, Tuple
+from pyqir.generator import Builder, SimpleModule, Value, types
 import unittest
+
+_BINARY_INTRINSICS: List[Tuple[str, Callable[[Builder], Callable[[Value, Value], Value]]]] = [
+    ("and", lambda b: b.and_),
+    ("or", lambda b: b.or_),
+    ("xor", lambda b: b.xor),
+    ("add", lambda b: b.add),
+    ("sub", lambda b: b.sub),
+    ("mul", lambda b: b.mul),
+    ("shl", lambda b: b.shl),
+    ("lshr", lambda b: b.lshr),
+    ("icmp eq", lambda b: b.icmp_eq),
+    ("icmp ne", lambda b: b.icmp_neq),
+    ("icmp ugt", lambda b: b.icmp_ugt),
+    ("icmp uge", lambda b: b.icmp_uge),
+    ("icmp ule", lambda b: b.icmp_ule),
+    ("icmp sgt", lambda b: b.icmp_sgt),
+    ("icmp sge", lambda b: b.icmp_sge),
+    ("icmp slt", lambda b: b.icmp_slt),
+    ("icmp sle", lambda b: b.icmp_sle)
+]
 
 
 class IntIntrinsicsTest(unittest.TestCase):
-    def test_int_intrinsics(self) -> None:
-        intrinsics = [
-            ("and", lambda b: b.and_, None),
-            ("or", lambda b: b.or_, None),
-            ("xor", lambda b: b.xor, None),
-            ("add", lambda b: b.add, None),
-            ("sub", lambda b: b.sub, None),
-            ("mul", lambda b: b.mul, None),
-            ("shl", lambda b: b.shl, None),
-            ("lshr", lambda b: b.lshr, None),
-            ("icmp eq", lambda b: b.icmp_eq, 1),
-            ("icmp ne", lambda b: b.icmp_neq, 1),
-            ("icmp ugt", lambda b: b.icmp_ugt, 1),
-            ("icmp uge", lambda b: b.icmp_uge, 1),
-            ("icmp ule", lambda b: b.icmp_ule, 1),
-            ("icmp sgt", lambda b: b.icmp_sgt, 1),
-            ("icmp sge", lambda b: b.icmp_sge, 1),
-            ("icmp slt", lambda b: b.icmp_slt, 1),
-            ("icmp sle", lambda b: b.icmp_sle, 1)
-        ]
-
-        for (name, build, width) in intrinsics:
+    def test_variable_variable(self) -> None:
+        for (name, build) in _BINARY_INTRINSICS:
             with self.subTest(name):
                 mod = SimpleModule("test " + name, 0, 0)
                 source = mod.add_external_function(
                     "source", types.Function([], types.INT))
-                ty = types.Integer(width) if width else types.INT
+                ty = types.BOOL if name.startswith("icmp") else types.INT
                 sink = mod.add_external_function(
                     "sink", types.Function([ty], types.VOID))
 
@@ -42,3 +43,32 @@ class IntIntrinsicsTest(unittest.TestCase):
                 mod.builder.call(sink, [z])
 
                 self.assertIn(f"%2 = {name} i64 %0, %1", mod.ir())
+
+    def test_constant_variable(self) -> None:
+        for (name, build) in _BINARY_INTRINSICS:
+            with self.subTest(name):
+                mod = SimpleModule("test " + name, 0, 0)
+                source = mod.add_external_function(
+                    "source", types.Function([], types.INT))
+                ty = types.BOOL if name.startswith("icmp") else types.INT
+                sink = mod.add_external_function(
+                    "sink", types.Function([ty], types.VOID))
+
+                x = mod.builder.call(source, [])
+                y = build(mod.builder)(1, x)
+                mod.builder.call(sink, [y])
+
+                self.assertIn(f"%2 = {name} i64 1, %1", mod.ir())
+
+    def test_neg(self) -> None:
+        mod = SimpleModule("test_neg", 0, 0)
+        source = mod.add_external_function(
+            "source", types.Function([], types.INT))
+        sink = mod.add_external_function(
+            "sink", types.Function([types.INT], types.VOID))
+
+        x = mod.builder.call(source, [])
+        y = mod.builder.neg(x)
+        mod.builder.call(sink, [y])
+
+        self.assertIn("%1 = sub i64 0, %1", mod.ir())
