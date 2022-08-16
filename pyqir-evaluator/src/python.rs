c239a81dc57f6b028a476cab5c10a6d0b38e487b
Sarah Marshall <sarah.marshall@microsoft.com>
2022-08-16 16:36:57 -0700

    Clippy is confused

--- pyqir-evaluator/src/python.rs
+++ pyqir-evaluator/src/python.rs
@@ -1,6 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+// pyo3 generates errors with _obj and _tmp values
+#![allow(clippy::used_underscore_binding)]
+// Some arguments get turned into Deref by PyO3 macros, which we can't control.
+#![allow(clippy::borrow_deref_ref, clippy::needless_option_as_deref)]
+
 use pyo3::{
     exceptions::PyOSError,
     prelude::*,
