643039605a8c10ac17eb92cb22183aea247d571f
Sarah Marshall <sarah.marshall@microsoft.com>
2022-11-02 17:37:00 -0700

    Update sphinx docs

---
+++ docs/api.md
@@ -0,0 +1,19 @@
+# API Reference
+
+## PyQIR
+
+```{eval-rst}
+.. automodule:: pyqir
+   :members:
+   :undoc-members:
+   :show-inheritance:
+```
+
+## Evaluation
+
+```{eval-rst}
+.. automodule:: pyqir.evaluator
+   :members:
+   :undoc-members:
+   :show-inheritance:
+```
--- docs/conf.py
+++ docs/conf.py
@@ -40,9 +40,6 @@ autodoc_type_aliases = {
     "Type": "Type",
 }
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
@@ -59,8 +56,3 @@ intersphinx_mapping = {
 # a list of builtin themes.
 #
 html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
