fcf46603dbdec5b297b355712b0675da37ec5bfa
Sarah Marshall <sarah.marshall@microsoft.com>
2022-11-14 10:55:06 -0800

    Update entry points test

--- pyqir/tests/test_entry_points.py
+++ pyqir/tests/test_entry_points.py
@@ -12,6 +12,7 @@ from pyqir import (
     Module,
     Type,
     create_entry_point,
+    is_entry_point,
     qubit,
     result,
 )
@@ -30,35 +31,28 @@ def bell(module: Module) -> Function:
     return entry
 
 
-def bell_no_measure(module: Module) -> Function:
-    entry = create_entry_point(module, "bell_no_measure", 2, 0)
+def external(module: Module) -> Function:
     context = module.context
-    builder = Builder(context)
-    builder.insert_from_end(BasicBlock(context, "", entry))
-    qis = BasicQisBuilder(builder)
-    qis.h(qubit(context, 0))
-    qis.cx(qubit(context, 0), qubit(context, 1))
-    return entry
-
-
-def using_external(module: Module) -> Function:
-    entry = create_entry_point(module, "using_external", 1, 0)
-    context = module.context
-    builder = Builder(context)
-    builder.insert_from_end(BasicBlock(context, "", entry))
     barrier = Function(
         FunctionType(Type.void(context), []),
         Linkage.EXTERNAL,
         "__quantum__qis__barrier__body",
         module,
     )
+
+    entry = create_entry_point(module, "barrier", 0, 0)
+    builder = Builder(context)
+    builder.insert_from_end(BasicBlock(context, "", entry))
     builder.call(barrier, [])
     return entry
 
 
 def test_multiple_entry_points() -> None:
-    module = Module(Context(), "multiple")
-    entries = [bell(module), bell_no_measure(module), using_external(module)]
-    print(module)
-    for i, entry in enumerate(entries):
-        print(f"{i + 1}. {entry.name}")
+    module = Module(Context(), "test")
+    bell_function = bell(module)
+    external_function = external(module)
+    entry_points = list(filter(is_entry_point, module.functions))
+    names = list(map(lambda f: f.name, entry_points))
+    assert len(entry_points) == 2
+    assert bell_function.name in names
+    assert external_function.name in names
