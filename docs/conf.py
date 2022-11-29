00e0d4e6551a3df098eb4361b798b9f12ba7fdeb
Sarah Marshall <sarah.marshall@microsoft.com>
2022-11-29 12:01:09 -0800

    Simplify docs structure

--- docs/conf.py
+++ docs/conf.py
@@ -4,19 +4,7 @@ author = "QIR Alliance"
 html_theme = "alabaster"
 exclude_patterns = ["_build"]
 
-extensions = [
-    "enum_tools.autoenum",
-    "myst_parser",
-    "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-]
-
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
 myst_enable_extensions = ["colon_fence"]
 myst_heading_anchors = 3
-
-intersphinx_mapping = {
-    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
-    "myst-parser": ("https://myst-parser.readthedocs.io/en/latest/", None),
-}
-
-autodoc_type_aliases = {"Type": "Type"}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
