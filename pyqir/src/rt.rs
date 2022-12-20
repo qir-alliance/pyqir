dcceaab77df090f1e008b10e07eaf214bf5144e8
Ian Davis <ian.f.davis@microsoft.com>
2022-12-20 15:30:04 -0800

    Fixing doc string

--- pyqir/src/rt.rs
+++ pyqir/src/rt.rs
@@ -75,7 +75,7 @@ pub(crate) fn initialize(py: Python, builder: Py<Builder>, data: &Value) -> PyRe
 ///
 /// :param Builder builder: The IR Builder used to create the instructions
 /// :param Value num_elements: How many tuple elements the tuple has
-/// :param str label: A string label for the tuple. Depending on the output schema, the label is included in the output or omitted.
+/// :param Value label: A string label for the tuple. Depending on the output schema, the label is included in the output or omitted.
 #[pyfunction]
 #[pyo3(text_signature = "(builder, num_elements, label)")]
 #[allow(clippy::needless_pass_by_value)]
