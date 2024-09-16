3c1bc8388a2647b26f745f88c36f23f2e728f914
Ian Davis <ian.f.davis@microsoft.com>
2024-09-16 09:01:52 -0700

    Python formatting

--- pyqir/tests/test_module_linking.py
+++ pyqir/tests/test_module_linking.py
@@ -21,11 +21,13 @@ def test_link_modules_with_same_context() -> None:
     expected_ir = str(Path("tests/combined_module.ll").read_text())
     assert actual_ir == expected_ir
 
+
 def test_link_modules_with_different_contexts() -> None:
     ir = Path("tests/random_bit.ll").read_text()
     dest = Module.from_ir(Context(), ir)
     ir = Path("tests/5_bit_random_number.ll").read_text()
     src = Module.from_ir(Context(), ir)
     message = dest.link(src)
-    assert message == "Cannot link modules from different contexts. Modules are untouched."
-
+    assert (
+        message == "Cannot link modules from different contexts. Modules are untouched."
+    )
