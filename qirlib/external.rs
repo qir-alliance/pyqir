2d86114c07a9022c4c2bc939a343482d7012e02f
Stefan J. Wernli <swernli@microsoft.com>
2025-04-03 11:16:53 -0700

    Fix panic when config path not set

--- qirlib/external.rs
+++ qirlib/external.rs
@@ -45,8 +45,13 @@ pub mod llvm_sys {
     /// Lazily searches for or compiles LLVM as configured by the environment
     /// variables.
     pub fn llvm_config(arg: &str) -> String {
-        llvm_config_ex(&*llvm_config_path().clone().unwrap(), arg)
-            .expect("Surprising failure from llvm-config")
+        llvm_config_ex(
+            &*llvm_config_path()
+                .clone()
+                .expect("should have configured location of llvm-config"),
+            arg,
+        )
+        .expect("Surprising failure from llvm-config")
     }
 
     /// Invoke the specified binary as llvm-config.
